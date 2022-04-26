81a53ea941c3dff443e0d0d97e1f2af7af90e63f
Sebastian Cruz <haloguy1175@gmail.com>
2022-04-26 10:46:14 -0700

    Initial Commit

---
+++ Bable Cookbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bable Cookbook
+//
+//  Created by Sebastian Cruz on 4/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
