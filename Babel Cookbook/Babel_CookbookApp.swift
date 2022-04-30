a6ed11a46ecb4be4d8eb079cd52830309e449dbf
Sebastian Cruz <haloguy1175@gmail.com>
2022-04-30 16:51:27 -0700

    Initial Commit

---
+++ Babel Cookbook/Babel_CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Babel_CookbookApp.swift
+//  Babel Cookbook
+//
+//  Created by Sebastian Cruz on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Babel_CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
